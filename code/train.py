b91048940c109d195b3a1bfdd994e67846d7a21a
Joseph DeGregorio <joseeph.degregorio@gmail.com>
2021-12-04 22:25:59 -0800

    Imported evalaution function for customization

--- code/train.py
+++ code/train.py
@@ -1,11 +1,21 @@
+# Standard imports
+import sys
+import os
+
+# 3rd party imports
+import numpy as np
+import yaml
 import gym
 import gym_kuiper_escape
-import pygame
 from stable_baselines3 import DQN
-import numpy as np
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_util import make_vec_env
-import yaml
+
+# Local imports
+path = os.getcwd() + '/code/'
+sys.path.insert(0, path)
+from evaluation import evaluate_policy
 
 # Load parameters
 with open('params.yaml', 'r') as f:
@@ -19,9 +29,27 @@ env_id = 'kuiper-escape-base-v0'
 n_cpu = 4
 
 # Create environment
-env = gym.make(env_id)  # single cpu
-# env = make_vec_env(env_id, n_envs=n_cpu)  # multiple cpus
+env = gym.make(env_id)
+env = Monitor(env, './')  # Monitor logs
 
 # Create model
 model = DQN('MlpPolicy', env, verbose=1, **model_params)
 
+# Evaluate baseline
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100, render=False)
+
+# Test drive dummy model
+env = model.get_env()
+all_episode_rewards = []
+for i in range(10):
+    episode_rewards = []
+    done = False
+    obs = env.reset()
+    while not done:
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        episode_rewards.append(reward)
+    all_episode_rewards.append(sum(episode_rewards))
+
+mean_episode_reward = np.mean(all_episode_rewards)
+print("Mean reward:", mean_episode_reward)
