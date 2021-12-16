0b0e4a5cb96caa4b50b2a05e108d07d4035d4e1b
Joseph DeGregorio <joseeph.degregorio@gmail.com>
2021-12-15 20:44:15 -0800

    Removed dead code and unused imports

--- train.py
+++ train.py
@@ -1,20 +1,15 @@
-# Standard imports
-import sys
-import os
-import time
-import random
+"""
+The purpose of this script is to train an AI agent to play the custom-built
+Kuiper Escape game using the A2C reinforcement learning algorithm.
+"""
 
 # 3rd party imports
-import numpy as np
-import yaml
 import gym
 import gym_kuiper_escape
+
 # from code.evaluation import evaluate_policy
 from stable_baselines.common.evaluation import evaluate_policy
-from code.callback import CustomEvalCallback
-from stable_baselines.common.callbacks import BaseCallback
 from stable_baselines.common.callbacks import EvalCallback
-from stable_baselines.common.policies import CnnLstmPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import A2C
 
@@ -41,16 +36,3 @@ agent.learn(5000000,
     callback=eval_callback
 )
 agent.save("agent")
-
-agent = A2C.load('./logs/agent_best/best_model.zip')
-evaluate_policy(agent, env_eval, n_eval_episodes=1, render=True)
-
-env_test = gym.make('kuiper-escape-base-v0')
-obs = env_test.reset()
-zero_completed_obs = np.zeros((agent.n_envs,) + agent.observation_space.shape)
-zero_completed_obs[0, :] = obs
-obs = zero_completed_obs
-for i in range(100):
-    action, _states = agent.predict(obs)
-    obs, rewards, dones, info = env_test.step(action[0])
-    env_test.render()
